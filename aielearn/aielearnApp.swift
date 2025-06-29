02afdd9a86bde70ae212f1687f4d1f1d1eb221a5
dinh.duong <jinhduong@users.noreply.github.com>
2025-06-29 14:00:44 +0800

    Initial Commit

---
+++ aielearn/aielearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  aielearnApp.swift
+//  aielearn
+//
+//  Created by Dinh Duong on 29/6/25.
+//
+
+import SwiftUI
+
+@main
+struct aielearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ aielearnTests/aielearnTests.swift
@@ -0,0 +1,17 @@
+//
+//  aielearnTests.swift
+//  aielearnTests
+//
+//  Created by Dinh Duong on 29/6/25.
+//
+
+import Testing
+@testable import aielearn
+
+struct aielearnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
